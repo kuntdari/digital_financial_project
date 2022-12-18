4c414518bf78a10d4ce1282f8ea6852092acad5e
kuntdari <kuntdari@korea.ac.kr>
2022-12-18 17:47:50 +0900

    first commit

---
+++ gitbash.sh
@@ -0,0 +1,8 @@
+echo "# digital_financial_project" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kuntdari/digital_financial_project.git
+git push -u origin main
+//
